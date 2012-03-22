3720811ca4a752bedbf6eb2699999b04db41055c
Luis Lopez <mikeylopez@gmail.com>
2012-03-21 20:54:33 -0400

    rem static data

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,14 @@
 <?php
 
 
-$db = mysql_connect("localhost", "root", "testing450311");
+$db = mysql_connect("", "", "");
 
-$sdb = mysql_select_db("usablex");
+$sdb = mysql_select_db("");
 
 //mysql_set_charset('utf8',$db);
 
 //mysql_query("SET NAMES utf8",$db);
 
-//define("SERVER_ONLINE", "10.10.40.16");
-//define("SERVER_OFFLINE","localhost");
 
 
 
